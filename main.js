0fdc8fff8a74f108bfa3de640f28af0b259a70d8
thibautvincent <thibautvincent@icloud.com>
2022-04-06 21:03:52 +0200

    Some refactoring and updating promos

--- main.js
+++ main.js
@@ -1,28 +1,72 @@
 ((document) => {
-
-    const secondHand = document.querySelector('.hand-second');
-    const minuteHand = document.querySelector('.hand-minute');
-    const hourHand = document.querySelector('.hand-hour');
-    const promos = {
-        9: {title : '21u-23u<br/>10 pinten = €10'},
-        10: {title : '21u-23u<br/>10 pinten = €10'},
-		11: {title : '23u-00u<br/>Glas Blue Thrill = €2'},
-		0: {title : '00u-01u<br/>Strongbow = €2'},
-		1: {title : '01u-02u<br/>Shotjes = €1'},
-		2: {title : '02u-03u<br/>Ginto = €4'},
-		3: {title : '03u-04u<br/>Sangria = €1,40'},
-	};
+    const promos = [
+        {
+            'start': '21:30',
+            'end': '23:00',
+            'title': '10 pinten',
+            'price': '€10',
+        },
+        {
+            'start': '00:00',
+            'end': '01:00',
+            'title': 'Rouge',
+            'price': '€2,5',
+        },
+        {
+            'start': '01:00',
+            'end': '02:00',
+            'title': 'Vodka Redbull',
+            'price': '€3',
+        },
+        {
+            'start': '02:00',
+            'end': '03:00',
+            'title': 'Passoa',
+            'price': '€3',
+        },
+        {
+            'start': '03:00',
+            'end': '04:00',
+            'title': 'Blue Thrill',
+            'price': '€2',
+        },
+    ];
 
     let rotations = [0, 0, 0]; // [seconds, minutes, hours]
 
-    function setTime() {
-        const now = new Date();
+    function getCurrentPromo(now) {
+        for (let index in promos) {
+            const promo = promos[index];
+            let startTime = new Date(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${promo.start}`);
+            let endTime = new Date(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${promo.end}`);
+            if (startTime <= now && now <= endTime) {
+                return promo;
+            }
+        }
+        return false;
+    }
+
+    function updateInterface() {
+        let now = new Date();
+
+        //now = new Date(`${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()} 22:05`);
+
+        const currentPromo = getCurrentPromo(now);
+        if (currentPromo !== false && now.getMinutes() % 5 == 0) {
+            showAction(currentPromo);
+            return;
+        }
+
+        hideAction();
+
+        updateClock(now);
+    }
+
+    function updateClock(now) {
 
         const seconds = now.getSeconds();
         const minutes = now.getMinutes();
         const hours = now.getHours() % 12;
-        // const hours = 3, minutes = 15;
-
 
         if (seconds === 0) {
             rotations[0]++;
@@ -36,27 +80,26 @@
             rotations[2]++;
         }
 
-		if (promos.hasOwnProperty(hours) && (minutes%15 === 0)) {
-            showAction(promos[hours].title.toString());
-        } else {
-            hideAction();
-        }
-
         const secondsDeg = (seconds / 60 * 360) + (rotations[0] * 360);
         const minutesDeg = (minutes / 60 * 360) + (rotations[1] * 360);
         const hoursDeg = (hours / 12 * 360) + (minutes / 60 * 30) + (rotations[2] * 360);
+
+        const secondHand = document.querySelector('.hand-second');
+        const minuteHand = document.querySelector('.hand-minute');
+        const hourHand = document.querySelector('.hand-hour');
 
         secondHand.style.transform = `rotate(${secondsDeg}deg)`;
         minuteHand.style.transform = `rotate(${minutesDeg}deg)`;
         hourHand.style.transform = `rotate(${hoursDeg}deg)`;
     }
 
-    function showAction(title) {
+    function showAction(promo) {
         document.getElementById('clock').style.display = 'none';
         document.body.style.backgroundImage = "url('images/promo_background.jpg')";
-        document.body.style.backgroundSize= 'cover';
+        document.body.style.backgroundSize = 'cover';
         var spanElement = document.getElementById('promo');
-        spanElement.innerHTML = title;
+        const content = `${promo.start} - ${promo.end}</br>${promo.title} = ${promo.price}`;
+        spanElement.innerHTML = content;
         spanElement.style.display = 'block';
     }
 
@@ -72,6 +115,6 @@
 
     }
 
-    setTime();
-    setInterval(setTime, 1000);
+    updateInterface();
+    setInterval(updateInterface, 1000);
 })(document);
